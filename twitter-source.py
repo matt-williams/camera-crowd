f7af3dddb29c0982abac543e746e5103d54df089
Mark Thomas <markbt@efaref.net>
2015-06-27 14:56:06 +0100

    Add notify-daemon

--- twitter-source.py
+++ twitter-source.py
@@ -39,8 +39,8 @@ def extract_media_url(data):
                     cur = conn.cursor() 
                     cur.execute('SELECT * from photos WHERE url=?;', (url,))
                     if cur.fetchone() is None:
-                        cur.execute('INSERT INTO photos(url, created, json) VALUES (?, ?, ?);',
-                                    (url, created_at.isoformat(), ''))
+                        cur.execute('INSERT INTO photos(url, created) VALUES (?, ?, ?);',
+                                    (url, created_at.isoformat()))
                     conn.commit()
                     conn.close()
 
