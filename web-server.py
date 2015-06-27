5473a0081b1cc503050792f4003b8b185c9863f1
Matt Williams <matwilliams@hotmail.com>
2015-06-27 18:38:14 +0100

    Disable debugging

--- web-server.py
+++ web-server.py
@@ -56,4 +56,4 @@ def events():
     return Response(gen(), mimetype="text/event-stream")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10080, threaded=True, debug=True)
+    app.run(host="0.0.0.0", port=10080, threaded=True, debug=False)
